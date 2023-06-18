975feca524241a31649dffae3a4edc3bb37b0c67
Simon <sstur@me.com>
2023-06-18 14:18:33 -0700

    Separate type from t() and export it

--- src/t.ts
+++ src/t.ts
@@ -1,15 +1,17 @@
 import { resolvePlaceholders } from './resolvePlaceholders';
 import { ExtractPlaceholders, MaybeValues } from './types';
 
+export type Translate = <S extends string>(
+  text: S,
+  ...[values]: MaybeValues<ExtractPlaceholders<S>>
+) => string;
+
 /**
  * This function doesn't actually do anything besides a bit of string
  * interpolation. Importantly though, it serves as a way to "mark" user-facing
  * strings in the codebase, so they can be statically extracted and translated
  * offline.
  */
-export function t<S extends string>(
-  text: S,
-  ...[values]: MaybeValues<ExtractPlaceholders<S>>
-) {
+export const t: Translate = (text, ...[values]) => {
   return values ? resolvePlaceholders(text, values) : text;
-}
+};
