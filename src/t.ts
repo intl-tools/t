efba244c846972768d71057bd9a43dfa4ce21a5c
Simon <sstur@me.com>
2023-06-18 14:12:34 -0700

    Update description in docblock for t()

--- src/t.ts
+++ src/t.ts
@@ -2,10 +2,10 @@ import { resolvePlaceholders } from './resolvePlaceholders';
 import { ExtractPlaceholders, MaybeValues } from './types';
 
 /**
- * This function doesn't actually do anything besides a bit of string formatting
- * (see `interpolate` below). Importantly though, it serves as a way to "mark"
- * string literals in the codebase, so they can be statically extracted and
- * translated offline.
+ * This function doesn't actually do anything besides a bit of string
+ * interpolation. Importantly though, it serves as a way to "mark" user-facing
+ * strings in the codebase, so they can be statically extracted and translated
+ * offline.
  */
 export function t<S extends string>(
   text: S,
